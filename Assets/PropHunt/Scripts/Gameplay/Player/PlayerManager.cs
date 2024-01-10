9b263f65fe36ea0d0b459476f1d5e5a7ebe201c6
Arnaud <arnaud.dellam@gmail.com>
2024-01-10 18:13:55 +0100

    Fix all probleme

--- Assets/PropHunt/Scripts/Gameplay/Player/PlayerManager.cs
+++ Assets/PropHunt/Scripts/Gameplay/Player/PlayerManager.cs
@@ -31,12 +31,20 @@ public class PlayerManager : NetworkBehaviour
         {
             lock (_lock)
             {
-                _life.Value += value;
-                print(Life);
-                if (_life.Value == 0 && NetworkManager.Singleton.IsHost)
+                _life.Value = _life.Value + value;
+                //print("Life Hunter="+isHunter.Value+": "+_life.Value);
+                /*print(NetworkManager.Singleton.ConnectedClientsIds.Count);
+                print(NetworkManager.Singleton.IsHost);
+                print(NetworkManager.Singleton.IsServer);
+                print(NetworkManager.Singleton.IsClient);*/
+                if (NetworkManager.Singleton.IsServer)
                 {
-                    ImDeadServerRpc(NetworkManager.Singleton.LocalClientId);
+                    if (_life.Value == 0)
+                    {
+                        this.GetComponent<NetworkObject>().Despawn();
+                    }
                 }
+
             }
         }
     }
@@ -51,6 +59,7 @@ public class PlayerManager : NetworkBehaviour
         if(_hunterController == null)
         {
             _hunterController = GetComponentInChildren<HunterController>();
+            print(_hunterController);
         }
         if(_actionInput == null)
         {
@@ -61,6 +70,7 @@ public class PlayerManager : NetworkBehaviour
     public override void OnNetworkSpawn()
     {
         base.OnNetworkSpawn();
+        _hunterController.Deactivate();
         isHunter.OnValueChanged += (@previousValue, @newValue) => SwapTeam();
         if (IsOwner)
         {
@@ -108,19 +118,5 @@ public class PlayerManager : NetworkBehaviour
         bool isLocked = !_movementController.cursorLocked;
         Cursor.lockState = isLocked? CursorLockMode.Locked : CursorLockMode.None;
         _movementController.cursorLocked = isLocked;
-    }
-
-    [ServerRpc]
-    private void ImDeadServerRpc(ulong clientId)
-    {
-        var client = NetworkManager.Singleton.ConnectedClients[clientId];
-        var playerobject = client.PlayerObject;
-        playerobject.Despawn();
-        Destroy(playerobject.gameObject);
-
-        if (GameManager.Instance.playerList.OneMoreDeath(client))
-        {
-            print("Fin du jeu");
-        }
     }
 }
